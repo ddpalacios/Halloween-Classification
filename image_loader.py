112185ab3845789841d40dec12479ee0975331e0
daniel <palaciosdanieldario@gmail.com>
2019-10-14 09:09:33 -0500

    configure some global variables

--- image_loader.py
+++ image_loader.py
@@ -11,3 +11,14 @@ ap.add_argument("-q", "--query", required=True,
 ap.add_argument("-o", "--output", required=True,
                 help="path to output directory of images")
 args = vars(ap.parse_args())
+
+
+# set your Microsoft Cognitive Services API key along with (1) the
+# maximum number of results for a given search and (2) the group size
+# for results (maximum of 50 per request)
+API_KEY = "YOUR_API_KEY_GOES_HERE"
+MAX_RESULTS = 250
+GROUP_SIZE = 50
+
+# set the endpoint API URL
+URL = "https://api.cognitive.microsoft.com/bing/v7.0/images/search"

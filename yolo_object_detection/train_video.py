aa998caece6580369444480ac05fb29b6ce3c409
ddpalacios <45171403+ddpalacios@users.noreply.github.com>
2019-12-06 23:44:06 -0600

    Add files via upload

---
+++ yolo_object_detection/train_video.py
@@ -0,0 +1,14 @@
+from imageai.Detection import VideoObjectDetection
+import os
+
+execution_path = os.getcwd()
+
+detector = VideoObjectDetection()
+detector.setModelTypeAsYOLOv3()
+detector.setModelPath( os.path.join(execution_path , "yolo.h5"))
+detector.loadModel()
+
+video_path = detector.detectObjectsFromVideo(input_file_path=os.path.join( execution_path, "demon3.MOV"),
+                                output_file_path=os.path.join(execution_path, "demonstration3")
+                                , frames_per_second=29, log_progress=True)
+print(video_path)
